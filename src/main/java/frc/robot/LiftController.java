4e91851ccb564660c4f8ba2beb199c021838f599
CyberGaming424 <31971253+CyberGaming424@users.noreply.github.com>
2019-02-11 23:03:35 -0500

    Finished manual control

--- src/main/java/frc/robot/LiftController.java
+++ src/main/java/frc/robot/LiftController.java
@@ -10,7 +10,7 @@ public class LiftController {
 
 	WPI_TalonSRX _lift = new WPI_TalonSRX(30);
 	Joystick _joy1;
-	double multiplier;
+	int step = 1;
 	// PID values
 	double P = 0.0004, I = 0, D = 0;
 	// CTREEnocder weird stuff
@@ -22,28 +22,53 @@ public class LiftController {
     double[] hatchHeight = new double[]{4096, 8192};
     enum hatchStage{
         STAGE1, STAGE2, STAGE3
-    }
+	}
+	hatchStage hatch;
 
 	// Height for placing the ball as well as modes to reach those points
     double[] ballHeight = new double[]{5000, 7000};
     enum ballStage{
         CARGOSHIP, STAGE1, STAGE2, STAGE3
-    }
+	}
+	ballStage ball;
 
 	// Modes to place the hatch and the ball
     enum Mode{
-        HATCH, BALL
+        MANUAL, AUTOHATCH, AUTOBALL
 	}
+	Mode mode;
 	
 	public LiftController(boolean setInverted, Joystick joy){
 		invert(setInverted);
 		_joy1 = joy;
 		liftPID.reset();
-    	liftPID.setOutputRange(-0.5, 0.5);
+		liftPID.setOutputRange(-0.5, 0.5);
+		hatch = null;
+		ball = null;
+		mode = null;
 	}
 
 	public void run(){
+		// Manual control
+		if(_joy1.getRawButton(3) || _joy1.getRawButton(4)){
+			mode = Mode.MANUAL;
+		}else if(_joy1.getRawButton(7) || _joy1.getRawButton(8) || _joy1.getRawButton(9))
+			mode = Mode.AUTOHATCH;
+		else if(_joy1.getRawButton(10)
+		 || _joy1.getRawButton(11) || _joy1.getRawButton(12) || _joy1.getRawButton(13))
+		 	mode = Mode.AUTOBALL;
 
+		 switch(mode){
+			case MANUAL:
+				manualControl();
+				break;
+			case AUTOHATCH:
+				autoHatch();
+				break;
+			case AUTOBALL:
+				autoBall();
+				break;
+		}
 	}
 
 	// Used to invert the talon direction
@@ -52,20 +77,29 @@ public class LiftController {
 	}
 	
 	public double get() {
-		double distance = _lift.getSelectedSensorPosition(0) * multiplier;
+		double distance = _lift.getSelectedSensorPosition(0);
         return distance;
 	}
-	
-	public void autoControl(Mode mode){
-		switch(mode){
-			case HATCH:
-				break;
-			case BALL:
-		}
+
+	public void autoHatch(){
+		if(_joy1.getRawButton(7)
 	}
-    
-    public void manualControl(double speed){
-		_lift.set(speed);
+	
+	public void autoBall(){
+
+	}
+
+    public void manualControl(){
+		if(_joy1.getRawButton(3)){
+			_lift.set(.5);
+			liftPID.setSetpoint(get());
+		}
+		else if(_joy1.getRawButton(4)){
+			_lift.set(-.3);
+			liftPID.setSetpoint(get());
+		}else
+			liftPID.enable();
+		
 	}
 
 	
